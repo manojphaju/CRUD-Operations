3024e15949c71680883bd234ac4a3b54dbc1036c
manojphaju <phajumanoj@gmail.com>
2018-12-19 12:40:39 +0545

    CRUD Operations

---
+++ MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        
+        
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength =3)]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name ="Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Required]
+        [Range(0,9)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+    }
+
+   
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
